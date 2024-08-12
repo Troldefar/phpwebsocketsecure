1da814e493d6b74481249446291c4b00224f4b24
troldefar <r_emil@live.dk>
2024-08-12 23:20:19 +0200

    std alias push

--- ws/websocket/src/Connector.php
+++ ws/websocket/src/Connector.php
@@ -4,34 +4,36 @@ namespace ws\websocket\src;
 
 class Connector {
 
+    private static $client;
+
     private const WAIT_FOR_MESSAGE_KEY = 'wait';
 
     public static function sendToServer(mixed $message = Constants::DEFAULT_CLIENT_MESSAGE) {
-        $client = self::tryConnect();
-        if (!$client) return;
+        self::$client = self::tryConnect();
+        if (!self::$client) return;
 
-        self::sendWebSocketMessage($client, $message);
+        self::sendWebSocketMessage($message);
 
-        if (str_contains($message, self::WAIT_FOR_MESSAGE_KEY)) return self::waitForMessage($client);
+        if (str_contains($message, self::WAIT_FOR_MESSAGE_KEY)) return self::waitForMessage();
 
-        fclose($client);
+        fclose(self::$client);
     }
 
-    private static function waitForMessage($client) {
-        return self::waitForResponse($client);
+    private static function waitForMessage() {
+        return self::waitForResponse();
     }
 
     /**
      * In case you want something back
      */
 
-    private static function waitForResponse($client) {
+    private static function waitForResponse() {
         $startTime = time();
         $timeout = 10;
         $buffer = '';
 
         while (time() - $startTime < $timeout) {
-            $data = self::getWebsocketMessage($client);
+            $data = self::getWebsocketMessage();
 
             if ($data) {
                 $buffer .= $data;
@@ -45,35 +47,38 @@ class Connector {
     }
 
     private static function tryConnect(): mixed {
+        try {
+            $websocketConfigs = Constants::getConfigs();
 
-        $websocketConfigs = Constants::getConfigs();
+            $serverConfig = new ServerConfig(
+                address: $websocketConfigs->address, 
+                port: $websocketConfigs->port, 
+                certFile: $websocketConfigs->paths->cert,
+                keyFile: $websocketConfigs->paths->key
+            );
 
-        $serverConfig = new ServerConfig(
-            address: $websocketConfigs->address, 
-            port: $websocketConfigs->port, 
-            certFile: $websocketConfigs->paths->cert,
-            keyFile: $websocketConfigs->paths->key
-        );
+            $context = $serverConfig->getBackendClientStreamContext();
+            $address = $serverConfig->getAddress() . ':' . $serverConfig->getPort();
 
-        $context = $serverConfig->getBackendClientStreamContext();
-        $address = $serverConfig->getAddress() . ':' . $serverConfig->getPort();
+            self::$client = @stream_socket_client('ssl://' . $address, $errno, $errstr, null, STREAM_CLIENT_CONNECT, $context);
 
-        $client = stream_socket_client('ssl://' . $address, $errno, $errstr, null, STREAM_CLIENT_CONNECT, $context);
+            if (!self::$client) {
+                app()->getResponse()->ok("Failed to connect: $errstr ($errno)\n");
+                return false;
+            }
 
-        if (!$client) {
-            app()->getResponse()->ok("Failed to connect: $errstr ($errno)\n");
-            return false;
+            if (!self::performHandshake()) {
+                fclose(self::$client);
+                return false;
+            }
+
+            return self::$client;
+        } catch (\Throwable $exception) {
+            var_dump($exception->getMessage());
         }
-
-        if (!self::performHandshake($client)) {
-            fclose($client);
-            return false;
-        }
-
-        return $client;
     }
 
-    private static function performHandshake($client) {
+    private static function performHandshake() {
         $websocketConfigs = Constants::getConfigs();
 
         $key = base64_encode(openssl_random_pseudo_bytes(16));
@@ -81,8 +86,8 @@ class Connector {
         $handshaker = new HandshakeHandler();
         $headers = $handshaker->prepareBackendClientHeaders($key, $websocketConfigs);
 
-        fwrite($client, $headers);
-        $response = fread($client, 5000);
+        fwrite(self::$client, $headers);
+        $response = fread(self::$client, 5000);
 
         if (preg_match(Constants::ACK_RESPONSE, $response, $matches)) return true;
         
@@ -90,12 +95,12 @@ class Connector {
         return false;
     }
 
-    private static function sendWebSocketMessage($client, $message) {
-        fwrite($client, FrameHandler::encodeWebSocketFrame($message));
+    private static function sendWebSocketMessage($message) {
+        fwrite(self::$client, FrameHandler::encodeWebSocketFrame($message));
     }
 
-    private static function getWebsocketMessage($client) {
-        return fread($client, 5000);
+    private static function getWebsocketMessage() {
+        return fread(self::$client, 5000);
     }
 
 }
